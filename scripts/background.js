6afd20a87418ccf179cd0a5c89e2b28e71813de7
knightfury16 <suhaibknight@gmail.com>
2024-06-26 13:59:28 +0600

    toggle title functional

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,6 @@
 const sYoutube = "https://www.youtube.com/watch";
-const youtube = "http://www.youtube.com/watch"
+const youtube = "http://www.youtube.com/watch";
+let hideState = false;
 
 chrome.commands.onCommand.addListener((command) => {
     console.log("command", command);
@@ -7,11 +8,30 @@ chrome.commands.onCommand.addListener((command) => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             //only send the message if user in on youtube watch
             if (tabs[0].url.startsWith(sYoutube) || tabs[0].url.startsWith(youtube)) {
+
                 console.log("On yoututbe")
-                chrome.tabs.sendMessage(tabs[0].id, { action: "modifyTitle", newTitle: "" }, (response) => {
-                    console.log("Title modified:", response?.status);
-                });
+
+                if (!hideState) {
+                    chrome.tabs.sendMessage(tabs[0].id, { action: "hide"}, (response) => {
+                        console.log("Title modified:", response?.status);
+                    });
+                    hideState = !hideState;
+                }
+                else {
+                    chrome.tabs.sendMessage(tabs[0].id, { action: "show"}, (response) => {
+                        console.log("Title modified:", response?.status);
+                    });
+                    hideState = !hideState;
+                }
             }
         });
     }
 })
+
+
+//to set hideState from popup
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "setHideState") {
+    hideState = request.hideState;
+  }
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,8 @@
+let ytTitle = '';
+console.log("RUNNING!!");
+
 function modifyYouTubeTitle(newTitle = null) {
+
   const titleElement = document.querySelector("#title h1 yt-formatted-string");
   const tabTitle = document.querySelector("title");
 
@@ -6,26 +10,47 @@ function modifyYouTubeTitle(newTitle = null) {
 
   // Check if the title element exists
   if (titleElement) {
-    // Rename the tab title to youtube
     if (newTitle) {
       titleElement.textContent = newTitle;
-      tabTitle.textContent = `${newTitle}- Youtube`;
+      tabTitle.textContent = `${newTitle} - Youtube`;
+      ytTitle = newTitle;
+      chrome.runtime.sendMessage({ action: "setHideState", hideState: false });
     }
     // If no new title is provided, remove the title
     else {
+      ytTitle = titleElement.textContent;
+      console.log("title:  ", ytTitle);
       titleElement.textContent = "";
       tabTitle.textContent = "Youtube";
     }
   }
 }
 
+function visibilityControl(operation) {
+  const titleElement = document.querySelector("#title h1 yt-formatted-string");
+  if (operation === 'hide') {
+    // titleElement.style.display = "none";
+    modifyYouTubeTitle()
+  }
+  else if (operation === 'show') {
+    // titleElement.style.display = "block";
+    modifyYouTubeTitle(ytTitle);
+  }
+}
+
+//On startup
 
 // Listen for messages from the popup 
-chrome.runtime.onMessage.addListener( async (request, sender, sendResponse) => {
-  console.log("Recieved on background.js", request);
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.action === "modifyTitle") {
-    const {newTitle} = request;
+    const { newTitle } = request;
     modifyYouTubeTitle(newTitle);
-    sendResponse({ status: "Success" });
   }
+  else if (request.action === 'show') {
+    visibilityControl('show');
+  }
+  else if (request.action === 'hide') {
+    visibilityControl('hide');
+  }
+  sendResponse({ status: "Success" });
 });
