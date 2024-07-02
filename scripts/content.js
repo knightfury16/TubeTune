6a7f151499d31f1bc3e243b511ec356ee8d669d5
knightfury16 <suhaibknight@gmail.com>
2024-07-02 09:21:56 +0600

    made startUp return promise

--- scripts/content.js
+++ scripts/content.js
@@ -4,31 +4,35 @@ let hideState = false; // Track hide state locally
 let currentVideoId = '';
 
 function onStartUp() {
-  console.log("TubeTune Starting...");
-
-  const metadataElement = document.querySelector("ytd-watch-metadata");
-  if (metadataElement) {
-    currentVideoId = metadataElement.getAttribute('video-id');
-  }
-  observeVideoIdChanges(metadataElement);
-  setGlobalTitle();
-  chrome.runtime.sendMessage({ action: "setBadge", text: "Off"});
+  console.log('TubeTune Starting...');
+  return new Promise((resolve, reject) => {
+    const metadataElement = document.querySelector('ytd-watch-metadata');
+    if (metadataElement) {
+      currentVideoId = metadataElement.getAttribute('video-id');
+      observeVideoIdChanges(metadataElement);
+      setGlobalTitle();
+      chrome.runtime.sendMessage({ action: 'setBadge', text: 'Off' });
+      resolve(metadataElement);
+    } else {
+      reject();
+    }
+  });
 }
 
 // Observe changes to the video ID
 function observeVideoIdChanges(metadataElement) {
-  const observer = new MutationObserver((mutations) => {
+  const observer = new MutationObserver(mutations => {
     for (const mutation of mutations) {
       if (mutation.attributeName === 'video-id') {
-        console.log("Video ID attribute changed");
+        console.log('Video ID attribute changed');
         const newVideoId = metadataElement.getAttribute('video-id');
-        console.log("New video ID:", newVideoId, "Current video ID:", currentVideoId);
+        console.log('New video ID:', newVideoId, 'Current video ID:', currentVideoId);
         if (newVideoId !== currentVideoId) {
           currentVideoId = newVideoId;
           setGlobalTitle();
           modifyYouTubeTitle();
           if (hideState) {
-            console.log("Hide state is true, hiding title");
+            console.log('Hide state is true, hiding title');
             visibilityControl('hide');
           }
         }
@@ -37,27 +41,25 @@ function observeVideoIdChanges(metadataElement) {
   });
 
   observer.observe(metadataElement, { attributes: true });
-  console.log("Started observing video ID changes");
+  console.log('Started observing video ID changes');
 }
-
 
 function setGlobalTitle(modifiedTitle = null) {
   //getting the title from tab title, element title does not load properly
-  const titleElement = document.querySelector("title");
-  console.log("Title from title: ", titleElement.innerHTML);
+  const titleElement = document.querySelector('title');
+  console.log('Title from title: ', titleElement.innerHTML);
   if (titleElement) {
     if (modifiedTitle) {
       ytTitle = modifiedTitle;
     } else {
       ytTitle = titleElement.textContent;
     }
-    console.log("Global title set to: ", ytTitle);
+    console.log('Global title set to: ', ytTitle);
   }
 }
 
 function modifyYouTubeTitle(newTitle = null) {
-
-  const titleElement = document.querySelector("#title h1 yt-formatted-string");
+  const titleElement = document.querySelector('#title h1 yt-formatted-string');
 
   // Check if the title element exists
   if (titleElement) {
@@ -74,42 +76,38 @@ function modifyYouTubeTitle(newTitle = null) {
 }
 
 function visibilityControl(operation) {
-
-  const tabTitle = document.querySelector("title");
-  const titleElement = document.querySelector("#title h1 yt-formatted-string");
+  const tabTitle = document.querySelector('title');
+  const titleElement = document.querySelector('#title h1 yt-formatted-string');
   const fullScreenElement = document.getElementsByClassName('ytp-chrome-top')[0];
 
   if (operation === 'hide') {
-    titleElement.style.display = "none";
+    titleElement.style.display = 'none';
     fullScreenElement.style.visibility = 'hidden';
-    tabTitle.textContent = "Youtube";
+    tabTitle.textContent = 'Youtube';
     hideState = true;
-    chrome.runtime.sendMessage({ action: "setHideState", hideState: true });
-  }
-  else if (operation === 'show') {
-    titleElement.style.display = "block";
+    chrome.runtime.sendMessage({ action: 'setHideState', hideState: true });
+  } else if (operation === 'show') {
+    titleElement.style.display = 'block';
     fullScreenElement.style.visibility = 'visible';
     tabTitle.textContent = `${ytTitle} - Youtube`;
     hideState = false;
-    chrome.runtime.sendMessage({ action: "setHideState", hideState: false });
+    chrome.runtime.sendMessage({ action: 'setHideState', hideState: false });
   }
 }
 
 //On startup
 setTimeout(onStartUp, 1000); // let DOM elements load
 
-// Listen for messages from the popup 
+// Listen for messages from the popup
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  if (request.action === "modifyTitle") {
+  if (request.action === 'modifyTitle') {
     const { newTitle } = request;
     modifyYouTubeTitle(newTitle);
     visibilityControl('show');
-  }
-  else if (request.action === 'show') {
+  } else if (request.action === 'show') {
     visibilityControl('show');
-  }
-  else if (request.action === 'hide') {
+  } else if (request.action === 'hide') {
     visibilityControl('hide');
   }
-  sendResponse({ status: "Success" });
+  sendResponse({ status: 'Success' });
 });
