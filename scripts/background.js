a738d9b4fffcdd459969c3d980eadf95fbb45444
knightfury16 <suhaibknight@gmail.com>
2025-04-22 15:38:35 +0600

    format

--- scripts/background.js
+++ scripts/background.js
@@ -5,56 +5,56 @@ const badgeColorOn = 'green';
 const badgeColorOff = '#FF8A8A';
 
 chrome.commands.onCommand.addListener(command => {
-  console.log('command', command);
-  if (command === 'hide') {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      //only send the message if user in on youtube watch
-      if (tabs[0].url.startsWith(sYoutube) || tabs[0].url.startsWith(youtube)) {
-        console.log('On yoututbe');
+    console.log('command', command);
+    if (command === 'hide') {
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            //only send the message if user in on youtube watch
+            if (tabs[0].url.startsWith(sYoutube) || tabs[0].url.startsWith(youtube)) {
+                console.log('On yoututbe');
 
-        if (!hideState) {
-          chrome.tabs.sendMessage(tabs[0].id, { action: 'hide' }, response => {
-            console.log('Title modified:', response?.status);
-            setBadge('On', badgeColorOn);
-          });
-          hideState = !hideState;
-        } else {
-          chrome.tabs.sendMessage(tabs[0].id, { action: 'show' }, response => {
-            console.log('Title modified:', response?.status);
-            setBadge('Off');
-          });
-          hideState = !hideState;
-        }
-      }
-    });
-  }
+                if (!hideState) {
+                    chrome.tabs.sendMessage(tabs[0].id, { action: 'hide' }, response => {
+                        console.log('Title modified:', response?.status);
+                        setBadge('On', badgeColorOn);
+                    });
+                    hideState = !hideState;
+                } else {
+                    chrome.tabs.sendMessage(tabs[0].id, { action: 'show' }, response => {
+                        console.log('Title modified:', response?.status);
+                        setBadge('Off');
+                    });
+                    hideState = !hideState;
+                }
+            }
+        });
+    }
 });
 
 function setBadge(text, color) {
-  const badgeColor = color ?? badgeColorOff;
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    console.log('On yoututbe');
-    chrome.action.setBadgeText({
-      text: text,
-      tabId: tabs[0].id
+    const badgeColor = color ?? badgeColorOff;
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        console.log('On yoututbe');
+        chrome.action.setBadgeText({
+            text: text,
+            tabId: tabs[0].id
+        });
+        chrome.action.setBadgeBackgroundColor({
+            color: badgeColor,
+            tabId: tabs[0].id
+        });
     });
-    chrome.action.setBadgeBackgroundColor({
-      color: badgeColor,
-      tabId: tabs[0].id
-    });
-  });
 }
 
 //to set hideState from popup
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'setHideState') {
-    hideState = request.hideState;
-    const badgeText = hideState ? 'On' : 'Off';
-    const badgeColor = badgeText == 'On' ? badgeColorOn : badgeColorOff;
-    setBadge(badgeText, badgeColor);
-  }
-  if (request.action === 'setBadge') {
-    const text = request.text;
-    setBadge(text);
-  }
+    if (request.action === 'setHideState') {
+        hideState = request.hideState;
+        const badgeText = hideState ? 'On' : 'Off';
+        const badgeColor = badgeText == 'On' ? badgeColorOn : badgeColorOff;
+        setBadge(badgeText, badgeColor);
+    }
+    if (request.action === 'setBadge') {
+        const text = request.text;
+        setBadge(text);
+    }
 });
