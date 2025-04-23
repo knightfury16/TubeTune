148fb58bd03686842f73eaba26c4cefab1b0949b
knightfury16 <suhaibknight@gmail.com>
2025-04-23 22:27:18 +0600

    Added a local store, need to exoport it

---
+++ scripts/localStore.js
@@ -0,0 +1,15 @@
+const hideState = 'hideState'
+
+export function getHideState() {
+    chrome.storage.local.get([hideState]).then(result => {
+        const newTit = result.hideState;
+        console.log("Getting hide state:::", newTit)
+    });
+}
+
+// isHidden is a boolean
+export function setHideState(isHidden) {
+    chrome.storage.local.set({ hideState: isHidden }).then(() => {
+        console.log("New Hide State set to,", isHidden);
+    })
+}
