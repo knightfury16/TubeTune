1fae4ca38b6b3867fc2350cd2d70bc58cfaae706
knightfury16 <suhaibknight@gmail.com>
2025-04-29 17:57:16 +0600

    moved the popup code to a separate folder

---
+++ src/popup/popup.js
@@ -0,0 +1,25 @@
+const sYoutube = "https://www.youtube.com/watch";
+const youtube = "http://www.youtube.com/watch";
+
+document.getElementById("modifyButton").addEventListener("click", async () => {
+  event.preventDefault();
+  const newTitle = document.getElementById("newTitle").value;
+  console.log("TITLE:", newTitle);
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    //only send the message if user in on youtube watch
+    if (tabs[0].url.startsWith(sYoutube) || tabs[0].url.startsWith(youtube)) {
+      console.log("On yoututbe");
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "modifyTitle", newTitle: newTitle },
+        (response) => {
+          console.log("Title modified:", response?.status);
+          window.close();
+        },
+      );
+    } else {
+      console.log("not on yoututbe");
+      window.close();
+    }
+  });
+});
